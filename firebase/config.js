4f56e53505874979b888d0f2431778e036ae6b6d
Sergey <104456968+Solod-S@users.noreply.github.com>
2023-01-22 17:16:37 +0200

    firebase registration + auth + database + storage

---
+++ firebase/config.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import "firebase/storage";
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth/react-native";
+import { Firestore, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBePau1bw4N6sgGjWOkei4mKR2CUsmdrZ0",
+  authDomain: "photogram-cfce0.firebaseapp.com",
+  projectId: "photogram-cfce0",
+  storageBucket: "photogram-cfce0.appspot.com",
+  messagingSenderId: "314004100068",
+  appId: "1:314004100068:web:a8d36e79d56548fd13edf1",
+  measurementId: "G-6G14GYQ2TK",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export const fsbase = getFirestore(app);
---
+++ router.js
@@ -0,0 +1,43 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// import { MaterialIcons } from "@expo/vector-icons";
+// import { Button, TouchableOpacity, Image } from "react-native";
+
+import Home from "./screens/Home";
+import screens from "./screens";
+const { LoginScreen, RegistrationScreen } = screens;
+
+const AuthStack = createStackNavigator();
+const DashboardStack = createStackNavigator();
+
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator initialRouteName="Auth">
+        <AuthStack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <AuthStack.Screen
+          options={{ headerShown: false }}
+          name="Registration"
+          component={RegistrationScreen}
+        />
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <DashboardStack.Navigator initialRouteName="Auth">
+      <DashboardStack.Screen
+        options={{ headerShown: false }}
+        name="Home"
+        component={Home}
+      />
+    </DashboardStack.Navigator>
+  );
+};
+
+export default useRoute;
